7924a958b5ba131fa1167bc126d6bbbc4ce3d7ed
Joe DeGrand <degrandjoe@gmail.com>
2019-10-26 17:07:19 -0400

    Initial commit

--- ColorApp WatchKit Extension/ContentView.swift
+++ ColorApp WatchKit Extension/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var clickedColor = 1
+    @State var currentColor = Color.white
+    @State var didTap = false
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            ForEach(0 ..< 10) { item in
+                HStack {
+                ForEach(0 ..< 10) { square in
+                    Rectangle().padding(.horizontal, 0.0).foregroundColor(self.didTap ? Color.white : Color.blue).frame(width: 10, height: 10)
+        
+                }
+                }
+            }
+            HStack {
+            Rectangle().foregroundColor(Color(red: 1, green: 0, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
+                Rectangle().foregroundColor(Color(red: 1, green: 129/255, blue: 0, opacity: 1.0)).frame(width: 20, height: 20).border(Color.black, width: 2).gesture(TapGesture()
+                        .onEnded {
+                            self.didTap.toggle()
+                        }
+                    )
+                Rectangle().foregroundColor(Color(red: 1, green: 1, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
+                Rectangle().foregroundColor(Color(red: 0, green: 1, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
+                Rectangle().foregroundColor(Color(red: 0, green: 0, blue: 1, opacity: 1.0)).frame(width: 20, height: 20)
+                Rectangle().foregroundColor(Color(red: 1, green: 0, blue: 1, opacity: 1.0)).frame(width: 20, height: 20)
+            }
+        }
     }
 }
 
