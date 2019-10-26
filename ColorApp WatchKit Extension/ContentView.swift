7e60e6b0c402792244ae238a90a4e80d4a0ff200
Joe DeGrand <degrandjoe@gmail.com>
2019-10-26 19:33:54 -0400

    Functionality for choosing colors and placing them on squares in grid

--- ColorApp WatchKit Extension/ContentView.swift
+++ ColorApp WatchKit Extension/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State var clickedColor = 1
-    @State var currentColor = Color.white
+    @State public var currentColor = Color.white
     @State var didTap = false
-    @State var matrix = [[Color]](repeating: [Color](repeating: Color.orange, count: 10), count: 10)
+    @State var matrix = [[Color]](repeating: [Color](repeating: Color.white, count: 10), count: 10)
     var body: some View {
         VStack {
             ForEach(0 ..< 10) { item in
@@ -29,24 +29,78 @@ struct ContentView: View {
                 }
             }
             HStack {
-            Rectangle().foregroundColor(Color(red: 1, green: 0, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
-                Rectangle().foregroundColor(Color(red: 1, green: 129/255, blue: 0, opacity: 1.0)).frame(width: 20, height: 20).border(Color.black, width: 2)
+//                ColorPick(color: .red)
+//                 ColorPick(color: .orange)
+//                 ColorPick(color: .yellow)
+//                 ColorPick(color: .green)
+//                 ColorPick(color: .blue)
+//                 ColorPick(color: .purple)
+               Rectangle().foregroundColor(Color.red).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.red))
+               .gesture(TapGesture()
+                   .onEnded {
+                       self.currentColor = Color.red
+                   }
+               )
+                Rectangle().foregroundColor(Color.orange).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.orange))
                     .gesture(TapGesture()
                         .onEnded {
-                            self.didTap.toggle()
+                            self.currentColor = Color.orange
                         }
                     )
-                Rectangle().foregroundColor(Color(red: 1, green: 1, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
-                Rectangle().foregroundColor(Color(red: 0, green: 1, blue: 0, opacity: 1.0)).frame(width: 20, height: 20)
-                Rectangle().foregroundColor(Color(red: 0, green: 0, blue: 1, opacity: 1.0)).frame(width: 20, height: 20)
-                Rectangle().foregroundColor(Color(red: 1, green: 0, blue: 1, opacity: 1.0)).frame(width: 20, height: 20)
+                Rectangle().foregroundColor(Color.yellow).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.yellow))
+                .gesture(TapGesture()
+                    .onEnded {
+                        self.currentColor = Color.yellow
+                    }
+                )
+                Rectangle().foregroundColor(Color.green).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.green))
+                .gesture(TapGesture()
+                    .onEnded {
+                        self.currentColor = Color.green
+                    }
+                )
+                Rectangle().foregroundColor(Color.blue).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.blue))
+                .gesture(TapGesture()
+                    .onEnded {
+                        self.currentColor = Color.blue
+                    }
+                )
+                Rectangle().foregroundColor(Color.purple).frame(width: 20, height: 20).border(Color.black, width: getWidth(color: Color.purple))
+                .gesture(TapGesture()
+                    .onEnded {
+                        self.currentColor = Color.purple
+                    }
+                )
+            
             }
         }
+    }
+    func getWidth(color: Color) -> CGFloat {
+        if isChosen(color: color) {
+            return 2
+        } else {
+            return 0
+        }
+    }
+    func isChosen(color: Color) -> Bool {
+        return color == self.currentColor
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorPick: View {
+    var color: Color
+    var body: some View {
+        Rectangle().foregroundColor(color).frame(width: 20, height: 20)
+            .gesture(TapGesture()
+                                .onEnded {
+//                                    ContentView.currentColor = Color.orange
+                                }
+                            )
     }
 }
